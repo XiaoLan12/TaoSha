1c528e091bd2651aaf3315893899901e7f1303c7
bibuzhixiong <1433212545@qq.com>
2017-07-20 19:54:21 +0800

    modify

--- app/src/main/java/com/yizhisha/taosha/adapter/SelectYarnColorAdapter.java
+++ app/src/main/java/com/yizhisha/taosha/adapter/SelectYarnColorAdapter.java
@@ -2,7 +2,10 @@ package com.yizhisha.taosha.adapter;
 
 import android.content.Context;
 import android.support.annotation.Nullable;
+import android.util.Log;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.chad.library.adapter.base.BaseQuickAdapter;
 import com.chad.library.adapter.base.BaseViewHolder;
@@ -22,7 +25,41 @@ public class SelectYarnColorAdapter extends BaseQuickAdapter<String,BaseViewHold
         this.mContext=context;
     }
     @Override
-    protected void convert(BaseViewHolder helper, String goods) {
+    protected void convert(final BaseViewHolder helper, String goods) {
+
+            helper.getView(R.id.tv_add).setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View v) {
+
+                    try {
+                        TextView tv_num=    (TextView)helper.getView(R.id.tv_num);
+                        Log.e("RRR",tv_num.getText().toString().trim()+"--");
+                        int num1=Integer.getInteger(tv_num.getText().toString().trim());
+                        num1++;
+                        ((TextView) helper.getView(R.id.tv_num)).setText(num1+"--");
+                    }catch (Exception e){
+
+                    }
+
+                }
+            });
+        helper.getView(R.id.tv_reduce).setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                try {
+                TextView tv_num=    (TextView)helper.getView(R.id.tv_num);
+                String num=tv_num.getText().toString().trim();
+                int num1=Integer.getInteger(num);
+                if(num1!=1){
+                    num1--;
+                    ((TextView) helper.getView(R.id.tv_num)).setText(num1+"");
+                }
+                }catch (Exception e){
+
+                }
+
+            }
+        });
 
     }
 }
