67b564d80e9ebe455be77fbcc0ecbf69314025d0
WSLittleBrother <15072477250@163.com>
2017-09-03 17:29:22 +0800

    修改前五个bug

--- app/src/main/java/com/yizhisha/taosha/ui/me/fragment/MyOrderFragment.java
+++ app/src/main/java/com/yizhisha/taosha/ui/me/fragment/MyOrderFragment.java
@@ -127,12 +127,34 @@ public class MyOrderFragment extends BaseFragment<MyOrderPresenter> implements
                 switch (view.getId()){
                     case R.id.cancel_the_order_tv:
                         if(dataList.get(position) instanceof OrderFootBean) {
-                            OrderFootBean orderFootBean= (OrderFootBean) dataList.get(position);
-                            currStatus=orderFootBean.getStatus();
-                            Map<String, String> bodyMap = new HashMap<>();
-                            bodyMap.put("uid", String.valueOf(AppConstant.UID));
-                            bodyMap.put("orderno", String.valueOf(orderFootBean.getOrderno()));
-                            mPresenter.cancleOrder(bodyMap);
+                           final OrderFootBean orderFootBean= (OrderFootBean) dataList.get(position);
+                            new NormalAlertDialog.Builder(activity)
+                                    .setBoolTitle(false)
+                                    .setContentText("确认取消订单？")
+                                    .setContentTextSize(18)
+                                    .setLeftText("取消")
+                                    .setRightText("确认")
+                                    .setWidth(0.75f)
+                                    .setHeight(0.33f)
+                                    .setOnclickListener(new DialogInterface.OnLeftAndRightClickListener<NormalAlertDialog>() {
+                                        @Override
+                                        public void clickLeftButton(NormalAlertDialog dialog, View view) {
+                                            dialog.dismiss();
+                                        }
+
+                                        @Override
+                                        public void clickRightButton(NormalAlertDialog dialog, View view) {
+
+                                            currStatus=orderFootBean.getStatus();
+                                            Map<String, String> bodyMap = new HashMap<>();
+                                            bodyMap.put("uid", String.valueOf(AppConstant.UID));
+                                            bodyMap.put("orderno", String.valueOf(orderFootBean.getOrderno()));
+                                            mPresenter.cancleOrder(bodyMap);
+                                            dialog.dismiss();
+
+                                        }
+                                    }).build().show();
+
                         }
                         break;
                     case R.id.immediate_evaluation_tv:
