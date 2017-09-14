aaf86af273e0c461a89c47b138233564e43307e4
WSLittleBrother <15072477250@163.com>
2017-09-14 15:51:09 +0800

    修改了欢迎也状态栏

--- app/src/main/java/com/yizhisha/taosha/base/BaseActivity.java
+++ app/src/main/java/com/yizhisha/taosha/base/BaseActivity.java
@@ -12,13 +12,11 @@ import android.os.Bundle;
 import android.os.IBinder;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.Window;
-import android.view.WindowManager;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 
